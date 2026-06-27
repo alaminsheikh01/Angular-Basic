b0b23cdbabeca7ae9152672a392144febf14a1a7
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-06-27 18:04:11 +0200

    added routerLinkActive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MyComponent } from "./Components/my-component/my-component";
 import { Employee } from "./services/employee/employee";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
