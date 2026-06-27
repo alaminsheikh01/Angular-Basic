d6e3c258f02936797982eccf7d3bc0489215733d
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-06-27 18:00:38 +0200

    implement routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MyComponent } from "./Components/my-component/my-component";
 import { Employee } from "./services/employee/employee";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Employee],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
