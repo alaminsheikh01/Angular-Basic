e46ec480bd7ab70e4aee0fcbe7d1b18d9338c2eb
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-06-27 23:30:37 +0200

    added async

--- src/app/Components/my-component/my-component.ts
+++ src/app/Components/my-component/my-component.ts
@@ -1,21 +1,18 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ObservableService } from '../../services/observable-service';
 // decorator
 @Component({
   selector: 'app-my-component',
-  imports: [CommonModule],
+  standalone:true,
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './my-component.html',
   styleUrl: './my-component.css',
 })
 export class MyComponent implements OnInit {
-  users: any[] = [];
+  users$: any
   constructor(private service: ObservableService) {}
   ngOnInit(): void {
-    this.service.getUsers().subscribe({
-      next: (data) => {
-        this.users = data;
-      },
-    });
+    this.users$ = this.service.getUsers()
   }
 }
