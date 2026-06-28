d2a7466050ad4a28abc0254acd8cc6b6bae7d7f0
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-06-28 11:08:13 +0200

    if condition with signal

--- src/app/Components/my-component/my-component.ts
+++ src/app/Components/my-component/my-component.ts
@@ -1,29 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // decorator
 @Component({
   selector: 'app-my-component',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './my-component.html',
   styleUrl: './my-component.css',
 })
 export class MyComponent {
-  // courseName : string = 'Angular';
+  selected: string = '';
 
-  cityList = signal<string[]>(['Hamburg', 'Kiel']);
+  isStudentActive = signal<boolean>(true)
 
-  studentObj = signal<any>({
-    name: 'Alamin',
-    city: 'Kiel',
-  });
-
-  AddCityList(name: string) {
-    this.cityList.update((data) => [...data, name]);
-  }
-
-  changeCity() {
-    this.studentObj.update((data: any) => ({ ...data, city: 'Hamburg' }));
+  changeStatus(status:boolean){
+    this.isStudentActive.set(status)
   }
 }
