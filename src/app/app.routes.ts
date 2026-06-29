9de6aa3347cb506cc05d1facf5d7e2f976f9b7fe
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-06-29 12:09:24 +0200

    router added for game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { About } from './Components/about/about';
 import { Contact } from './Components/contact/contact';
 import { User } from './Components/user/user';
 import { Todo } from './Components/todo/todo';
+import { Game } from './Components/game/game';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
-  {path: 'user', component: User},
-  {path: 'todo', component:Todo},
+  { path: 'user', component: User },
+  { path: 'todo', component: Todo },
+  {path: 'game', component: Game},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: About },
 ];
+
